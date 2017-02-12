dab57cd77812fd8c77ead5d023213127b63291b5
spite400k <spite400k@gmail.com>
2017-02-12 23:45:49 +0900

    first

--- JsonFormatWepApp/src/main/java/com/example/jsonFormat/JsonFormatController.java
+++ JsonFormatWepApp/src/main/java/com/example/jsonFormat/JsonFormatController.java
@@ -3,7 +3,6 @@ package com.example.jsonFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class JsonFormatController {
@@ -15,8 +14,8 @@ public class JsonFormatController {
 
     @RequestMapping("/format")
     public String format(Model model) {
-    	
-    	
+
+
     	return "JsonFormat/jsonFormat";
     }
 }
